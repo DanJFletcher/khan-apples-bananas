7aab5d431fcd49aeffa9cd4a29bfa549140a6685
Dan Fletcher <dannyjfletcher@outlook.com>
2016-03-25 15:03:45 +0000

    change hint code - step 1

--- validation.js
+++ validation.js
@@ -27,7 +27,7 @@ staticTest($._("Color those classes"), function() {
     var result = null;
     var descrip = $._("This webpage displays the lyrics for a song about apples and bananas. There are already classes on most of the words about apples and bananas, but there is no style for those classes yet. Add two style rules, one that sets a color for the 'apples' class and another that sets a color for the 'bananas' class.");
     
-    var displayP = " _ {\n   _: _;\n }"; 
+    var displayP = "<style>\n   _ {\n     _: _;\n   }\n</style>"; 
     
     var applesColorP = ".apples { color: $color; }";
     var applesBackgroundColorP = ".apples { background-color: $color; }";
